f01d902ebd418501ec210edc90745b569427a8ac
Michelle Dotzenrod <dotzenrodm@gmail.com>
2015-09-15 16:48:57 -0500

    2nd question Normal mode

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,15 @@ class UsersController < ApplicationController
     render text: user_array
   end
 
+  def show
+    begin
+      user = User.find(params[:id])
+      render text: "user: #{user.full_name}", status: 200
+    rescue ActiveRecord::RecordNotFound
+      render text: "Sorry, user with id of #{params[:id]} was not found", status: 404
+    end
+  end
+
+
+
 end
