cf1af436a562a68ffba2c4e341f0abc9cd54c21f
Michelle Dotzenrod <dotzenrodm@gmail.com>
2015-09-15 19:11:40 -0500

    combined index method if statements

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,16 @@
 class UsersController < ApplicationController
 
-  # def index
-  if @params[:id] == nil
-    user = User.all
-    user_array = user.all.map { |record| "#{record.first_name} #{record.last_name}, #{record.age}" }
-    render text: user_array
-  elsif @params[]
-    # render text: "params: #{params.inspect}""
-    user = User.where("first_name LIKE ?", "s%")
-    user_array_with_s = user.map { |user| "#{user.first_name} #{user.last_name}, #{user.age}" }
-    user_array_with_s = user_array_with_s.inspect
-    render text: "#{user_array_with_s}"
+  def index
+    if params.has_key?(:first_name)
+      user = User.where("first_name LIKE ?", "s%")
+      user_array_with_s = user.map { |user| "#{user.first_name} #{user.last_name}, #{user.age}" }
+      user_array_with_s = user_array_with_s.inspect
+      render text: "#{user_array_with_s}"
+    elsif params[:id] == nil
+      user = User.all
+      user_array = user.all.map { |record| "#{record.first_name} #{record.last_name}, #{record.age}" }
+      render text: user_array
+    end
   end
 
   def show
@@ -23,5 +23,13 @@ class UsersController < ApplicationController
   end
 
 
+# #keep these methods to show params only
+    # def index
+    #   render text: "params: #{params.inspect}"
+    # end
 
+    # def show
+    #   render text: "params: #{params.inspect}"
+    # end
 end
+
