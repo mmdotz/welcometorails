be48fc3b5d8a6f361694365ca5a6335f38a0c425
Michelle Dotzenrod <dotzenrodm@gmail.com>
2015-09-16 13:34:01 -0500

    clean up readme

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,20 +2,33 @@ class UsersController < ApplicationController
 
   def index
     if params.has_key?(:first_name)
-      user = User.where("first_name LIKE ?", "s%")
+      user = User.where("first_name LIKE ?", params[:first_name])
       user_array_with_s = user.map { |user| "#{user.first_name} #{user.last_name}, #{user.age}" }
       user_array_with_s = user_array_with_s.inspect
       render text: "#{user_array_with_s}"
-    elsif params[:id] == nil
+    end
+    if params[:id] == nil
       user = User.all
       user_array = user.all.map { |record| "#{record.first_name} #{record.last_name}, #{record.age}" }
       render text: user_array
     end
   end
 
-  def show
+  def show #see MAtt's notes in slack
     begin
       user = User.find(params[:id])
+      render text: "user: #{user.full_name}", status: 200
+    rescue ActiveRecord::RecordNotFound
+      render text: "Sorry, user with id of #{params[:id]} was not found", status: 404
+    end
+  end
+
+  def delete
+    if params.has_key?("delete")
+      user = User.find(params[:id])
+      user.destroy
+      render text: "user was deleted"
+    else
       render text: "user: #{user.full_name}", status: 200
     rescue ActiveRecord::RecordNotFound
       render text: "Sorry, user with id of #{params[:id]} was not found", status: 404
